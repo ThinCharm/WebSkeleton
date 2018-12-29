bfca351a2d9a298067cfb49b344bce670ffef4d1
ThinCharm <46181699+ThinCharm@users.noreply.github.com>
2018-12-29 02:52:03 -0500

    Update Encrypter.php

--- src/Engine/Encrypter.php
+++ src/Engine/Encrypter.php
@@ -1,1 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * WebSkeleton - A fast and secure web skeleton powered by Symfony.
+ * @author WebSkeleton Contributors <https://github.com/ThinCharm/WebSkeleton/graphs/contributors>
+ * @link <https://github.com/XChess> WebSkeleton.
+ */
 
+namespace ThinCharm\Engine;
+
+use ParagonIE\Halite\Asymmetric\Crypto;
+use ParagonIE\Halite\Symmetric\Crypto;
+use ParagonIE\Halite\KeyFactory:
+use ParagonIE\Halite\Cookie;
+use ParagonIE\Halite\File;
+
+/**
+ * The encrypter.
+ */
+class Encrypter implements EncrypterInterface
+{
+}
